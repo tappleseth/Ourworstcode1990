b09a9489757c5542693ff952022c063f60b2fb25
taftj <taft.justin@gmail.com>
2014-11-10 20:26:08 -0800

    Serial communications things added
    Added all of the functionallity and stuff to get the serial communications up and running

--- main.c
+++ main.c
@@ -24,6 +24,41 @@ unsigned int tempCount = 0;
 unsigned int frequencyCount = 0;
 unsigned int seed = 0;
 
+#include "driverlib/uart.h"
+
+
+// The UART interrupt handler.
+void
+UARTIntHandler(void)
+{
+    unsigned long ulStatus;
+    // Get the interrrupt status.
+    ulStatus = UARTIntStatus(UART0_BASE, true);
+
+    // Clear the asserted interrupts.
+    UARTIntClear(UART0_BASE, ulStatus);
+    
+    // Loop while there are characters in the receive FIFO.
+    while(UARTCharsAvail(UART0_BASE))
+    {
+        // Read the next character from the UART and write it back to the UART.
+        UARTCharPutNonBlocking(UART0_BASE, UARTCharGetNonBlocking(UART0_BASE));
+    }
+}
+
+
+// Send a string to the UART.
+void
+UARTSend(const unsigned char *pucBuffer, unsigned long ulCount)
+{    
+    // Loop while there are more characters to send.
+    while(ulCount--)
+    {      
+        // Write the next character to the UART.
+        UARTCharPutNonBlocking(UART0_BASE, *pucBuffer++);
+    }
+}
+
 int main(){
   Startup();
   //OKAY YOU FRAUDS, TIME TO IMPLEMENT AN ACTUAL STACK THINGY.
@@ -110,6 +145,24 @@ void Startup(void) {
   GPIOPinIntEnable(GPIO_PORTE_BASE, 0xF);
   IntEnable(INT_GPIOE);
   
+  //Enable Uart serial communications peripherals
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+  
+  // Enable processor interrupts.
+  IntMasterEnable();
+
+  // Set GPIO A0 and A1 as UART pins.
+  GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+  // Configure the UART for 115,200, 8-N-1 operation.
+  UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+  (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+   UART_CONFIG_PAR_NONE));
+
+  // Enable the UART interrupt.
+  IntEnable(INT_UART0);
+  UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
   
   return;
 }
@@ -167,7 +220,9 @@ void TomSchedule(){
   static char flairTitle1[] = "Applehansontaft \0";
   static char flairTitle2[] = "Discount Freight \0";
   RIT128x96x4StringDraw(flairTitle1, 10, 10, 15);
+  UARTSend((unsigned char *)flairTitle1, 16);
   RIT128x96x4StringDraw(flairTitle2, 10, 20, 15);
+  UARTSend((unsigned char *)flairTitle2, 17);
   //static int checkSize = 0;
   
   
