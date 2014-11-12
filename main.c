b57c2a5c2fc080da9718d613a6791571b1758c5d
Brandon Johanson <bkj1991@uw.edu>
2014-11-11 16:50:57 -0800

    Update main.c

--- main.c
+++ main.c
@@ -16,48 +16,14 @@
 #include "driverlib/gpio.h"
 #include "driverlib/pwm.h"
 #include "driverlib/interrupt.h"
+
 boringTom* head;
 boringTom* current;
 unsigned int TrainState = 0;
 unsigned int TimerState = 0;
+unsigned int seed = 0;
 unsigned int tempCount = 0;
 unsigned int frequencyCount = 0;
-unsigned int seed = 0;
-
-#include "driverlib/uart.h"
-
-
-// The UART interrupt handler.
-void
-UARTIntHandler(void)
-{
-    unsigned long ulStatus;
-    // Get the interrrupt status.
-    ulStatus = UARTIntStatus(UART0_BASE, true);
-
-    // Clear the asserted interrupts.
-    UARTIntClear(UART0_BASE, ulStatus);
-    
-    // Loop while there are characters in the receive FIFO.
-    while(UARTCharsAvail(UART0_BASE))
-    {
-        // Read the next character from the UART and write it back to the UART.
-        UARTCharPutNonBlocking(UART0_BASE, UARTCharGetNonBlocking(UART0_BASE));
-    }
-}
-
-
-// Send a string to the UART.
-void
-UARTSend(const unsigned char *pucBuffer, unsigned long ulCount)
-{    
-    // Loop while there are more characters to send.
-    while(ulCount--)
-    {      
-        // Write the next character to the UART.
-        UARTCharPutNonBlocking(UART0_BASE, *pucBuffer++);
-    }
-}
 
 int main(){
   Startup();
@@ -119,9 +85,6 @@ void Startup(void) {
   PWMGenEnable(PWM0_BASE, PWM_GEN_0);   
   PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, FALSE);
   
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
-  GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, PORT_DATA); 
-  
   // Set the clocking to run directly from the crystal.
   SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
                  SYSCTL_XTAL_8MHZ);
@@ -145,25 +108,27 @@ void Startup(void) {
   GPIOPinIntEnable(GPIO_PORTE_BASE, 0xF);
   IntEnable(INT_GPIOE);
   
-  //Enable Uart serial communications peripherals
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+  /*GPIO PORT D INTERRUPT SETUP*/
+    //Clear the default ISR handler and install IntGPIOe as the handler:
+  GPIOPortIntUnregister(GPIO_PORTD_BASE);
+  GPIOPortIntRegister(GPIO_PORTD_BASE,IntGPIOd);
   
-  // Enable processor interrupts.
-  IntMasterEnable();
-
-  // Set GPIO A0 and A1 as UART pins.
-  GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-  // Configure the UART for 115,200, 8-N-1 operation.
-  UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
-  (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
-   UART_CONFIG_PAR_NONE));
-
-  // Enable the UART interrupt.
-  IntEnable(INT_UART0);
-  UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+  //Enable GPIO port E, set pin 0 as an input
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);    
+  GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, 0x20);
   
+  //Activate the pull-up on GPIO port E
+  GPIOPadConfigSet(GPIO_PORTD_BASE, 0x20, GPIO_STRENGTH_2MA,
+                   GPIO_PIN_TYPE_STD_WPU);
+  
+  //Configure GPIO port E as triggering on falling edges
+  GPIOIntTypeSet(GPIO_PORTD_BASE, 0x20, GPIO_FALLING_EDGE);
+  
+  //Enable interrupts for GPIO port E
+  GPIOPinIntEnable(GPIO_PORTD_BASE, 0x20);
+  IntEnable(INT_GPIOD);
+  /*GPIO PORT D INTERRUPT SETUP*/  
+    
   return;
 }
 
@@ -220,9 +185,7 @@ void TomSchedule(){
   static char flairTitle1[] = "Applehansontaft \0";
   static char flairTitle2[] = "Discount Freight \0";
   RIT128x96x4StringDraw(flairTitle1, 10, 10, 15);
-  UARTSend((unsigned char *)flairTitle1, 16);
   RIT128x96x4StringDraw(flairTitle2, 10, 20, 15);
-  UARTSend((unsigned char *)flairTitle2, 17);
   //static int checkSize = 0;
   
   
@@ -260,20 +223,20 @@ void TomSchedule(){
       addToStack(&trainComHell);
     }
     while (current != NULL){
-      current->justTrainTaskThings(current->localDataPtr,current->globalDataPtr);
-      current = current->next;
-      while (!TimerState) {
-        
-      }
-      TimerState=0;
-      frequencyCount = tempCount;
-      //tempCount = 0;
-      
-      ourGlobalData.globalCount++;
+      current = NULL;
+      //current->justTrainTaskThings(current->localDataPtr,current->globalDataPtr);
+      //current = current->next;
     }
     
-  }
-  
+    while (!TimerState) {
+      //wait
+    }
+    
+    TimerState=0;
+    frequencyCount = tempCount;
+    tempCount = 0;
+    ourGlobalData.globalCount++;
+  } 
 }
 
 
@@ -322,6 +285,14 @@ void IntTimer0(void) {
   return;
 }
 
+void IntGPIOd(void)
+{
+  //Clear the interrupt to avoid continuously looping here
+  GPIOPinIntClear(GPIO_PORTD_BASE, 0x20);
+  
+  tempCount++;
+}
+
 //stolen wholesale from professor's provided code
 void IntGPIOe(void)
 {
@@ -333,7 +304,4 @@ void IntGPIOe(void)
   
   //Switches are normally-high, so flip the polarity of the results:
   TrainState=TrainState^0xF;  //You should work out why and how this works!
-  
-  if(TrainState == 1)
-    tempCount++;
 }
