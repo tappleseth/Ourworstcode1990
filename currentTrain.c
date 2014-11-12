24728c4fb97299096aa1156d337adef80288fd72
tappleseth <tomappleseth@gmail.com>
2014-11-12 12:41:46 -0800

    Flickering text bug fixed
    fixed flickering text bug

--- currentTrain.c
+++ currentTrain.c
@@ -15,7 +15,16 @@
 #include "driverlib/gpio.h"
 #include "driverlib/pwm.h"
 #include "driverlib/interrupt.h"
+
+/* 
+recently modified to account for edge case of simultaneous gridlock and train traversal 
+this fixes the flickering text bug, 
+which resulted from "GRIDLOCK" and "<TO direction>" overlapping
+oho!
+-Thomas
+*/
 void CurrentTrain(void* localData, void* sharedData){
+  //PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, TRUE);
   //produce OLED/audio alarms
   //remove from task queue if no train is present
   globalData* globalPtr = (globalData*)sharedData;    
@@ -26,40 +35,40 @@ void CurrentTrain(void* localData, void* sharedData){
   
   */
   
-  if(globalPtr->north == TRUE) {
-    #if (TASK_SELECT == 2 || TASK_SELECT == -1)
+  if(globalPtr->north == TRUE && !globalPtr->gridlock) {
+#if (TASK_SELECT == 2 || TASK_SELECT == -1)
     pin(HIGH);
-  #endif
+#endif
     
-  
-  
-  static unsigned int noiseCount = 0;
-  static unsigned int northFlashCount = 0;  
-  static char northDisplay[] = "NorthTrain \0";
-  static int brightness = 15;
-  
-  if(globalPtr->north) {
-    if(!localPtr->toggleNorth){
-      localPtr->toggleNorth = TRUE;
-      noiseCount = globalPtr->globalCount + 60;
-      northFlashCount = globalPtr->globalCount;
-      brightness = 15;
-    }
     
-    if(localPtr->toggleNorth) {
-      //FLASH EVENTS
-      //loops indefinitely (will end when train passes)
-      if((globalPtr->globalCount - northFlashCount) % 18 == 0)
-        brightness = 15; 
+    
+    static unsigned int noiseCount = 0;
+    static unsigned int northFlashCount = 0;  
+    static char northDisplay[] = "Heading: North \0";
+    static int brightness = 15;
+    
+    if(globalPtr->north) {
+      if(!localPtr->toggleNorth){
+        localPtr->toggleNorth = TRUE;
+        noiseCount = globalPtr->globalCount + 60;
+        northFlashCount = globalPtr->globalCount;
+        brightness = 15;
+      }
       
-      if((globalPtr->globalCount - northFlashCount) % 18 == 9)
-        brightness = 0;
-      
-      RIT128x96x4StringDraw(northDisplay, 10, 40, brightness); 
-      //END FLASH EVENTS
-      
-      //SOUND EVENTS
-      #if SOUND_ENABLE == 1
+      if(localPtr->toggleNorth) {
+        //FLASH EVENTS
+        //loops indefinitely (will end when train passes)
+        if((globalPtr->globalCount - northFlashCount) % 18 == 0)
+          brightness = 15; 
+        
+        if((globalPtr->globalCount - northFlashCount) % 18 == 9)
+          brightness = 0;
+        
+        RIT128x96x4StringDraw(northDisplay, 10, 40, brightness); 
+        //END FLASH EVENTS
+        
+        //SOUND EVENTS
+#if SOUND_ENABLE == 1
         //first blast: lasts 2 seconds
         if(globalPtr->globalCount == (noiseCount - 60))
           PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, TRUE);
@@ -96,24 +105,24 @@ void CurrentTrain(void* localData, void* sharedData){
           noiseCount = globalPtr->globalCount + 60;
           PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, TRUE);
         }      
-      #endif
+#endif
+      }
     }
-  }
-  
-  //placeholder: need code to STOP SOME SHIT
-  if(globalPtr->globalCount >= globalPtr->traversalTime) {
-    PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, FALSE);
-    noiseCount = 0;
-    northFlashCount = 0;
-    brightness = 15;
-    localPtr->toggleNorth = FALSE;
-    globalPtr->currentTrainComplete = TRUE;
-    RIT128x96x4StringDraw(northDisplay, 10, 40, 0);
-  }
-  
-  #if TASK_SELECT == 2 || TASK_SELECT == -1
+    
+    //placeholder: need code to STOP SOME SHIT
+    /*if(globalPtr->globalCount >= globalPtr->traversalTime) {
+      PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, FALSE);
+      noiseCount = 0;
+      northFlashCount = 0;
+      brightness = 15;
+      localPtr->toggleNorth = FALSE;
+      globalPtr->currentTrainComplete = TRUE;
+      RIT128x96x4StringDraw(northDisplay, 10, 40, 0);
+    }*/
+    
+#if TASK_SELECT == 2 || TASK_SELECT == -1
     pin(LOW); 
-  #endif    
+#endif    
   }
   
   /*
@@ -122,40 +131,40 @@ void CurrentTrain(void* localData, void* sharedData){
   
   */
   
-  if(globalPtr->east == TRUE) {
-    #if TASK_SELECT == 4 || TASK_SELECT == -1
+  if(globalPtr->east == TRUE && !globalPtr->gridlock) {
+#if TASK_SELECT == 4 || TASK_SELECT == -1
     pin(HIGH);
-  #endif
-   
-  
-  static unsigned int eastNoiseCount = 0;
-  static unsigned int eastFlashCount = 0;
-  static char eastDisplay[] = "EastTrain \0";
-  static int brightness = 15;
-  
-  if(globalPtr->east) {
-    if(!localPtr->toggleEast) {
-      localPtr->toggleEast = TRUE;
-      eastNoiseCount = globalPtr->globalCount + 78;
-      eastFlashCount = globalPtr->globalCount;
-      brightness = 15;
-    }
+#endif
     
-    //*****FLASH EVENTS*****  
     
-    if(localPtr->toggleEast) {
-      //FLASH OFF
+    static unsigned int eastNoiseCount = 0;
+    static unsigned int eastFlashCount = 0;
+    static char eastDisplay[] = "Heading: East \0";
+    static int brightness = 15;
+    
+    if(globalPtr->east) {
+      if(!localPtr->toggleEast) {
+        localPtr->toggleEast = TRUE;
+        eastNoiseCount = globalPtr->globalCount + 78;
+        eastFlashCount = globalPtr->globalCount;
+        brightness = 15;
+      }
       
-      if ((globalPtr->globalCount - eastFlashCount) % 12 == 0)
-        brightness = 15; 
+      //*****FLASH EVENTS*****  
       
-      if ((globalPtr->globalCount - eastFlashCount) % 12 == 6)
-        brightness = 0;
-      
-      RIT128x96x4StringDraw(eastDisplay, 10, 40, brightness); 
-      
-      //*****SOUND EVENTS*****
-      #if SOUND_ENABLE == 1
+      if(localPtr->toggleEast) {
+        //FLASH OFF
+        
+        if ((globalPtr->globalCount - eastFlashCount) % 12 == 0)
+          brightness = 15; 
+        
+        if ((globalPtr->globalCount - eastFlashCount) % 12 == 6)
+          brightness = 0;
+        
+        RIT128x96x4StringDraw(eastDisplay, 10, 40, brightness); 
+        
+        //*****SOUND EVENTS*****
+#if SOUND_ENABLE == 1
         //first blast: lasts 2 seconds
         if(globalPtr->globalCount == (eastNoiseCount - 78)) 
           PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, TRUE);
@@ -200,23 +209,23 @@ void CurrentTrain(void* localData, void* sharedData){
           eastNoiseCount = globalPtr->globalCount + 78;
           PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, TRUE);
         }
-      #endif
-    }   
-  }
-  
-  if(globalPtr->globalCount >= globalPtr->traversalTime) {
-    PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, FALSE);
-    globalPtr->currentTrainComplete = TRUE;
-    eastNoiseCount = 0;
-    eastFlashCount = 0;
-    brightness = 15;
-    localPtr->toggleEast = FALSE;
-    RIT128x96x4StringDraw(eastDisplay, 10, 40, 0);
-  }
-
-  #if TASK_SELECT == 4 || TASK_SELECT == -1
+#endif
+      }   
+    }
+    
+   /* if(globalPtr->globalCount >= globalPtr->traversalTime) {
+      PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, FALSE);
+      globalPtr->currentTrainComplete = TRUE;
+      eastNoiseCount = 0;
+      eastFlashCount = 0;
+      brightness = 15;
+      localPtr->toggleEast = FALSE;
+      RIT128x96x4StringDraw(eastDisplay, 10, 40, 0);
+    }*/
+    
+#if TASK_SELECT == 4 || TASK_SELECT == -1
     pin(LOW);
-  #endif    
+#endif    
   }
   
   /*
@@ -225,37 +234,37 @@ void CurrentTrain(void* localData, void* sharedData){
   
   */
   
-  if(globalPtr->west == TRUE) {
-    #if TASK_SELECT == 3 || TASK_SELECT == -1
+  if(globalPtr->west == TRUE && !globalPtr->gridlock) {
+#if TASK_SELECT == 3 || TASK_SELECT == -1
     pin(HIGH);
-  #endif
-
-  
-  static unsigned int westNoiseCount = 0;
-  static unsigned int westFlashCount = 0;
-  static char westDisplay[] = "WestTrain \0";
-  static int brightness = 15;  
-  
-  if(globalPtr->west) {
-    if(!localPtr->toggleWest) {
-      localPtr->toggleWest = TRUE;
-      westNoiseCount = globalPtr->globalCount + 42;
-      westFlashCount = globalPtr->globalCount;
-      brightness = 15;
-    }
+#endif
     
-    //FLASH EVENTS
-    if(localPtr->toggleWest) {
-      if ((globalPtr->globalCount - westFlashCount) % 24 == 0)
-        brightness = 15; 
+    
+    static unsigned int westNoiseCount = 0;
+    static unsigned int westFlashCount = 0;
+    static char westDisplay[] = "Heading: West \0";
+    static int brightness = 15;  
+    
+    if(globalPtr->west) {
+      if(!localPtr->toggleWest) {
+        localPtr->toggleWest = TRUE;
+        westNoiseCount = globalPtr->globalCount + 42;
+        westFlashCount = globalPtr->globalCount;
+        brightness = 15;
+      }
       
-      if ((globalPtr->globalCount - westFlashCount) % 24 == 12)
-        brightness = 0;
-      
-      RIT128x96x4StringDraw(westDisplay, 10, 40, brightness); 
-      
-      //SOUND EVENTS
-      #if SOUND_ENABLE == 1      
+      //FLASH EVENTS
+      if(localPtr->toggleWest) {
+        if ((globalPtr->globalCount - westFlashCount) % 24 == 0)
+          brightness = 15; 
+        
+        if ((globalPtr->globalCount - westFlashCount) % 24 == 12)
+          brightness = 0;
+        
+        RIT128x96x4StringDraw(westDisplay, 10, 40, brightness); 
+        
+        //SOUND EVENTS
+#if SOUND_ENABLE == 1      
         //first blast: lasts 2 seconds
         if(globalPtr->globalCount == (westNoiseCount - 42))
           PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, TRUE);
@@ -284,23 +293,23 @@ void CurrentTrain(void* localData, void* sharedData){
           westNoiseCount = globalPtr->globalCount + 42;
           PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, TRUE);
         }
-      #endif
+#endif
+      }
     }
-  }
-  
-  if(globalPtr->globalCount >= globalPtr->traversalTime) {
-    PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, FALSE);
-    globalPtr->currentTrainComplete = TRUE;
-    westNoiseCount = 0;
-    westFlashCount = 0;
-    brightness = 15;
-    localPtr->toggleWest = FALSE;
-    RIT128x96x4StringDraw(westDisplay, 10, 40, 0);
-  }
-  
-  #if TASK_SELECT == 3 || TASK_SELECT == -1
+    
+    /*if(globalPtr->globalCount >= globalPtr->traversalTime) {
+      PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, FALSE);
+      globalPtr->currentTrainComplete = TRUE;
+      westNoiseCount = 0;
+      westFlashCount = 0;
+      brightness = 15;
+      localPtr->toggleWest = FALSE;
+      RIT128x96x4StringDraw(westDisplay, 10, 40, 0);
+    }*/
+    
+#if TASK_SELECT == 3 || TASK_SELECT == -1
     pin(LOW);
-  #endif    
+#endif    
   }
   
   /*
@@ -309,39 +318,39 @@ void CurrentTrain(void* localData, void* sharedData){
   
   */
   
-  if(globalPtr->south == TRUE) {
-    #if TASK_SELECT == 5 || TASK_SELECT == -1
+  if(globalPtr->south == TRUE && !globalPtr->gridlock) {
+#if TASK_SELECT == 5 || TASK_SELECT == -1
     pin(HIGH);
-  #endif
-     
-  static unsigned int southNoiseCount = 0;
-  static unsigned int southFlashCount = 0;
-  static char southDisplay[] = "SouthTrain \0";
-  static int brightness = 15;
-  
-  if(globalPtr->south) {
-    if(!localPtr->toggleSouth) {
-      localPtr->toggleSouth = TRUE;
-      southNoiseCount = globalPtr->globalCount + 72;
-      southFlashCount = globalPtr->globalCount;
-      brightness = 15;
-    }
+#endif
     
-    //*****FLASH EVENTS*****  
+    static unsigned int southNoiseCount = 0;
+    static unsigned int southFlashCount = 0;
+    static char southDisplay[] = "Heading: South \0";
+    static int brightness = 15;
     
-    if(localPtr->toggleSouth) {
-      //FLASH OFF
-      
-      if ((globalPtr->globalCount - southFlashCount) % 12 == 0)
+    if(globalPtr->south) {
+      if(!localPtr->toggleSouth) {
+        localPtr->toggleSouth = TRUE;
+        southNoiseCount = globalPtr->globalCount + 72;
+        southFlashCount = globalPtr->globalCount;
         brightness = 15;
+      }
       
-      if ((globalPtr->globalCount - southFlashCount) % 12 == 6)
-        brightness = 0;
+      //*****FLASH EVENTS*****  
       
-      RIT128x96x4StringDraw(southDisplay, 10, 40, brightness); 
-      
-      //*****SOUND EVENTS*****
-      #if SOUND_ENABLE == 1
+      if(localPtr->toggleSouth) {
+        //FLASH OFF
+        
+        if ((globalPtr->globalCount - southFlashCount) % 12 == 0)
+          brightness = 15;
+        
+        if ((globalPtr->globalCount - southFlashCount) % 12 == 6)
+          brightness = 0;
+        
+        RIT128x96x4StringDraw(southDisplay, 10, 40, brightness); 
+        
+        //*****SOUND EVENTS*****
+#if SOUND_ENABLE == 1
         //first blast: lasts 2 seconds
         if(globalPtr->globalCount == (southNoiseCount - 72)) 
           PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, TRUE);
@@ -386,23 +395,33 @@ void CurrentTrain(void* localData, void* sharedData){
           southNoiseCount = globalPtr->globalCount + 72;
           PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, TRUE);
         }
-      #endif
-    }   
-  }
-  
-  if(globalPtr->globalCount >= globalPtr->traversalTime) {
-    PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, FALSE);
-    globalPtr->currentTrainComplete = TRUE;
-    southNoiseCount = 0;
-    southFlashCount = 0;
-    brightness = 15;
-    localPtr->toggleSouth = FALSE;
-    RIT128x96x4StringDraw(southDisplay, 10, 40, 0);
-  }
-
-  #if TASK_SELECT == 4 || TASK_SELECT == -1
+#endif
+      }   
+    }
+    
+    /*if(globalPtr->globalCount >= globalPtr->traversalTime) {
+      PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, FALSE);
+      globalPtr->currentTrainComplete = TRUE;
+      southNoiseCount = 0;
+      southFlashCount = 0;
+      brightness = 15;
+      localPtr->toggleSouth = FALSE;
+      RIT128x96x4StringDraw(southDisplay, 10, 40, 0);
+    }*/
+    
+#if TASK_SELECT == 4 || TASK_SELECT == -1
     pin(LOW);
-  #endif    
+#endif    
+  }
+  //NOTE TO SELF: BUUUULLLSHIIIIIT
+  if(globalPtr->globalCount >= globalPtr->traversalTime) {
+    //PWMOutputState(PWM0_BASE, PWM_OUT_1_BIT, FALSE);
+      localPtr->toggleSouth = FALSE;
+      localPtr->toggleWest = FALSE;
+      localPtr->toggleEast = FALSE;
+      localPtr->toggleNorth = FALSE;
+      globalPtr->currentTrainComplete = TRUE;
+      
   }
   return;
 }
