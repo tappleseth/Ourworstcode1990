02a88ae4b9e0858855b2d645e0e96473837359f5
tappleseth <tomappleseth@gmail.com>
2014-11-12 18:07:33 -0800

    Create UART_support.c

---
+++ UART_support.c
@@ -0,0 +1,59 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "headerThingy.h"
+
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+
+#include "inc/lm3s8962.h"
+#include "driverlib/debug.h"
+#include "driverlib/sysctl.h"
+#include "drivers/rit128x96x4.h"
+#include "driverlib/timer.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pwm.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/uart.h"
+
+void UARTIntHandler(void)
+{
+    unsigned long ulStatus;
+
+    //
+    // Get the interrrupt status.
+    //
+    ulStatus = UARTIntStatus(UART0_BASE, true);
+
+    //
+    // Clear the asserted interrupts.
+    //
+    UARTIntClear(UART0_BASE, ulStatus);
+
+    //
+    // Loop while there are characters in the receive FIFO.
+    //
+    while(UARTCharsAvail(UART0_BASE))
+    {
+        //
+        // Read the next character from the UART and write it back to the UART.
+        //
+        UARTCharPutNonBlocking(UART0_BASE, UARTCharGetNonBlocking(UART0_BASE));
+    }
+}
+
+void UARTSend(const unsigned char *pucBuffer, unsigned long ulCount)
+{
+    //
+    // Loop while there are more characters to send.
+    //
+    while(ulCount--)
+    {
+        //
+        // Write the next character to the UART.
+        //
+        UARTCharPutNonBlocking(UART0_BASE, *pucBuffer++);
+    }
+}
