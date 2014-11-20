ab398ef6082f7e2976bbbabee5a34ec4bde58c3e
tappleseth <tomappleseth@gmail.com>
2014-11-19 17:37:59 -0800

    Create random.c

---
+++ random.c
@@ -0,0 +1,29 @@
+#include "headerThingy.h"
+
+int randomInteger(int low, int high) {
+  double randNum = 0.0;
+  int multiplier = 2743;
+  int addOn = 5923;
+  double max = INT_MAX + 1.0;
+  
+  int retVal = 0;
+  
+  if (low > high)
+    retVal = randomInteger(high, low);
+  else
+  {
+    seed = seed*multiplier + addOn;
+    randNum = seed;
+    
+    if (randNum <0)
+    {
+      randNum = randNum + max;
+    }
+    
+    randNum = randNum/max;
+    
+    retVal = ((int)((high-low+1)*randNum))+low;
+  }
+  
+  return retVal;
+}
